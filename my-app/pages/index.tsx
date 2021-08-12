ce6248935567974735cc37ff3195d1962703108b
sreeharimv10 <sreeharimv10@gmail.com>
2021-08-12 11:47:59 +0530

    NextJS Pokemon Search

--- my-app/pages/index.tsx
+++ my-app/pages/index.tsx
@@ -7,14 +7,14 @@ export default function Home(){
     const [pokemon ,putPokemon] = useState<any[]>([])
     const [query,setQuery]=useState('');
 
-    const searchPokemon = (e: { preventDefault: () => void; }) =>{
+    /*const searchPokemon = (e: { preventDefault: () => void; }) =>{
         e.preventDefault();
         console.log('Searched for', query);
         fetch('https://pokeapi.co/api/v2/pokemon/${query}').then((resp)=>resp.json()).then(data=>{
             putPokemon(data);
             console.log(data);
         });
-    };
+    };*/
 
     console.log(pokemon, 'pokemon state')
 
@@ -31,16 +31,27 @@ export default function Home(){
 				                <div className="col-sm-3 text-center p-3" style= {{background:'blue'}}>
                         </div>
                         <div className='col-md-9'>
-					                  <form className="mb-0  d-flex justify-content-center align-items-center h-100 w-100"
-                            onSubmit={searchPokemon}>
+					                  <form className="mb-0  d-flex justify-content-center align-items-center h-100 w-100">
 						                    <div>
 							                      <input id="search_name"
-                                    type="text"
-                                    value={query}
-                                    onChange={(e)=>{setQuery(e.currentTarget.value);}}
-                                    placeholder="Enter Pokemon Name"/>
+                                                    type="text"
+                                                    value={query}
+                                                    onChange={(e)=>{setQuery(e.currentTarget.value);}}
+                                                    placeholder="Enter Pokemon Name"/>
 
-                                    <input id="submit_button" type="submit" value="Search"
+                                                    <input id="submit_button" 
+                                                        onClick={(e)=>
+                                                        {
+                                                            e.preventDefault();
+                                                            console.log("searched for "+query)
+                                                            fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+                                                            .then((resp)=>resp.json())
+                                                            .then(data=>{
+                                                                            putPokemon(data);
+                                                                            console.log(data);
+                                                                        });
+                                                        }}
+                                    type="submit" value="Search"
                                     className="btn btn-light p-2"/>
 						                    </div>
 					                  </form>
