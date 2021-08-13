e83581aba03cf9d7a5319f09db81dda6081781a3
sreeharimv10 <sreeharimv10@gmail.com>
2021-08-13 15:37:41 +0530

    NextJS Pokemon Search

--- searchpokemon/next.config.js
+++ searchpokemon/next.config.js
@@ -2,3 +2,5 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+
--- searchpokemon/pages/pokemon/[pokemon].tsx
+++ searchpokemon/pages/pokemon/[pokemon].tsx
@@ -1,26 +1,32 @@
 import React, { useEffect, useState } from 'react'
-import Image from 'next/image'
+import Image from 'next/dist/client/image';
 {/*@ts-ignore*/}
-const PokemonPage = ({name}) =>
+const PokemonPage = ({name, pokemon, error}) =>
 {
-    const [pokemon,setPokemon] = useState<any[]>([])
+    // Client Side Rendering
+    /*const [pokemon,setPokemon] = useState<any[]>([])
 
 
     const searchPokemon = () =>
     {
         fetch(`https://pokeapi.co/api/v2/pokemon/${name}`).then((resp)=>resp.json()).then(data=>{
             setPokemon(data);
-        }).catch((err) => 
+        }).catch((err) =>
         {
             alert('ErroR')
         })
     };
 
     // eslint-disable-next-line react-hooks/exhaustive-deps
-    useEffect(searchPokemon, []);
+    useEffect(searchPokemon, []);*/
+
+    return error ?
+        <div>
+            <h1>Pokemon not found</h1>
+        </div>:
 
 
-    return <div>
+        <div>
                 {/*@ts-ignore*/}
                 <h1>{pokemon?.name}</h1>
                 {/*@ts-ignore*/}
@@ -37,17 +43,21 @@ const PokemonPage = ({name}) =>
                 )}
                 {/*@ts-ignore*/}
                 {pokemon?.sprites &&(
-                    // eslint-disable-next-line jsx-a11y/alt-text
-                    <Image src={/*@ts-ignore*/
-                    pokemon.sprites.front_default}/>
+                    // eslint-disable-next-line @next/next/no-img-element
+                    <img src={/*@ts-ignore*/pokemon.sprites.front_default}/>
                     )}
         </div>
 }
-
+// Server Side Rendering
 {/*@ts-ignore*/}
 PokemonPage.getInitialProps = ({query}) =>
 {
-    return {name: query.name};
+    return fetch(`https://pokeapi.co/api/v2/pokemon/${query?.pokemon}`).then((resp)=>resp.json()).then(data=>{
+        return {name: query.pokemon, pokemon: data};
+    }).catch((err) =>
+    {
+        return {error: err}
+    })
 };
 
 export default PokemonPage;
