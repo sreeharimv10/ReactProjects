49f127a6fd78c58204c02dd2cb13cbe70acdb2f5
sreeharimv10 <sreeharimv10@gmail.com>
2021-08-30 15:45:22 +0530

    Pokedex API

--- pokemonsearch/pages/index.js
+++ pokemonsearch/pages/index.js
@@ -1,6 +1,8 @@
 import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import logo from '../images/pokemon_logo.png'
+
 
 
 export default function Home()
@@ -26,37 +28,35 @@ export default function Home()
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"/>
                 </Head>
             <main>
-                <div id="search-box" className="bg-primary w-100 p-2" style={{backgroundColor:"blue"}}>
-			              <div className="row m-0">
-				                <div className="col-sm-3 text-center p-3" style= {{background:'blue'}}>
-                        </div>
-                        <div className='col-md-9'>
-					                  <form className="mb-0  d-flex justify-content-center align-items-center h-100 w-100"
-                                      >
-						                    <div>
-							                      <input id="search_name"
-                                                    type="text"
-                                                    value={query}
-                                                    onChange={(e)=>{setQuery(e.currentTarget.value);}}
-                                                    placeholder="Enter Pokemon Name"/>
+                <div id="search-box" className="bg-primary w-100 p-2" style={{backgroundColor:"blue", padding:20}}>
+			        <div className="row m-0">
+                        <Image src={logo} alt="Pokemon" height="60" width="160"/>
+                    </div>
+                    <div className='col-md-9'>
+					    <form className="mb-0  d-flex justify-content-center align-items-center h-100 w-100">
+						    <div style={{textAlign:"right", paddingInlineEnd:290}}>
+							    <input id="search_name"
+                                    type="text"
+                                    value={query}
+                                    onChange={(e)=>{setQuery(e.currentTarget.value);}}
+                                    placeholder="Enter Pokemon Name"/>
 
-                                                    <input id="submit_button"
-                                                     onClick={(e)=>{
-                                                        e.preventDefault();
-                                                        console.log("searched for "+query)
-                                                        fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
-                                                        .then((resp)=>resp.json())
-                                                        .then(data=>
-                                                        {
-                                                            putPokemon(data);
-                                                            console.log(data);
-                                                        });}}
-                                                        type="submit" value="Search"
-                                                        className="btn btn-light p-2" />
+                                <input id="submit_button"
+                                    onClick={(e)=>{
+                                    e.preventDefault();
+                                    console.log("searched for "+query)
+                                    fetch(`https://pokeapi.co/api/v2/pokemon/${query}`)
+                                    .then((resp)=>resp.json())
+                                    .then(data=>
+                                    {
+                                        putPokemon(data);
+                                        console.log(data);
+                                    });}}
+                                    type="submit" value="Search"
+                                    className="btn btn-light p-2" />
 						                    </div>
-					                  </form>
-				                </div>
-			              </div>
+					    </form>
+				    </div>
                 </div>
                 <h1>{pokemon?.name}</h1>
 
