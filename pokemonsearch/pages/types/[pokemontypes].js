167f272ce533d5b7f53b1781c618354a87ad3cda
sreeharimv10 <sreeharimv10@gmail.com>
2021-09-06 19:12:42 +0530

    Pokedex API

--- pokemonsearch/pages/types/[pokemontypes].js
+++ pokemonsearch/pages/types/[pokemontypes].js
@@ -1,4 +1,4 @@
-/*import Link from 'next/link'
+import Link from 'next/link'
 import styled from '@emotion/styled'
 
 
@@ -36,43 +36,5 @@ const types=({pokemons})=>
     )
 }
 
-export default types*/
-export const getStaticPaths = async () => {
-    const res = await fetch('https://jsonplaceholder.typicode.com/users');
-    const data = await res.json();
-  
-    // map data to an array of path objects with params (id)
-    const paths = data.map(ninja => {
-      return {
-        params: { id: ninja.id.toString() }
-      }
-    })
-  
-    return {
-      paths,
-      fallback: false
-    }
-  }
-  
-  export const getStaticProps = async (context) => {
-    const id = context.params.id;
-    const res = await fetch('https://jsonplaceholder.typicode.com/users/' + id);
-    const data = await res.json();
-  
-    return {
-      props: { ninja: data }
-    }
-  }
-  
-  const Details = ({ ninja }) => {
-    return (
-      <div>
-        <h1>{ ninja.name }</h1>
-        <p>{ ninja.email }</p>
-        <p>{ ninja.website }</p>
-        <p>{ ninja.address.city }</p>
-      </div>
-    );
-  }
-  
-  export default Details;
+export default types
+
