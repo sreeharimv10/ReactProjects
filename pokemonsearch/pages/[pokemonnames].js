318602308ee4003591f8952b51d1135749670a54
sreeharimv10 <sreeharimv10@gmail.com>
2021-09-14 19:01:41 +0530

    Pokedex API

--- pokemonsearch/pages/[pokemonnames].js
+++ pokemonsearch/pages/[pokemonnames].js
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import Link from 'next/link'
 import styles from '../styles/Home.module.css'
 import styled from '@emotion/styled'
 import logo from '../images/pokemon_logo.png'
@@ -31,12 +32,13 @@ export default function Home() {
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css"/>
        </Head>
        <main>
-        <div id="search-box" className="bg-primary w-100 p-2" style={{backgroundColor:"blue", padding:15,
-                                                display: "flex"}}>
-			    <div className="row m-0" style={{justifyContent:"initial", paddingInlineStart:100}}>
-            <Image src={logo} alt="Pokemon" height="60" width="160" />
-          </div>
-        </div>
+       <nav className='flex flex-wrap items-center p-3 bg-blue-600 '>
+            <Link href='#'>
+                <a className='inline-flex items-center p-2 mr-4 '>
+                    <Image src={logo} alt="Logo" className="" width="190" height="70"/>
+                </a>
+            </Link>
+        </nav>
           <h1 style={{marginLeft: 32}}>Selected Pokemon: {selectedPokemon}</h1>
           <div>
           {pokemons?.map((p) =>
--- pokemonsearch/pages/types/[pokemontypes].js
+++ pokemonsearch/pages/types/[pokemontypes].js
@@ -1,5 +1,7 @@
 import Link from 'next/link'
+import Image from 'next/image'
 import styled from '@emotion/styled'
+import logo from '../images/pokemon_logo.png'
 
 
 export const getServerSideProps =async ()=>
@@ -14,8 +16,14 @@ export const getServerSideProps =async ()=>
 const types=({pokemons})=>
 {
     return(
+        <nav className='flex flex-wrap items-center p-3 bg-blue-600 '>
+            <Link href='#'>
+                <a className='inline-flex items-center p-2 mr-4 '>
+                    <Image src={logo} alt="Logo" className="" width="190" height="70"/>
+                </a>
+            </Link>
+            
         <div>
-        
         {
             pokemons.results.map((pokemon)=>{
                 return (
@@ -33,6 +41,7 @@ const types=({pokemons})=>
             )})
         }
         </div>
+        </nav>
     )
 }
 
